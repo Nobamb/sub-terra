246cbbe8723e0fa2fd278c19718a4d285924eba0
Nobamb <jsh12345644@gmail.com>
2026-07-31 17:13:57 +0900

    feat: 사다리 존 트리거로 플레이어 사다리 전환 추가

---
+++ sub-terra/Assets/_Project/Scripts/Gameplay/Player/LadderZone.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace SubTerra.Gameplay.Player
+{
+    /// <summary>플레이어가 사다리 Trigger 안에 있을 때만 중력 없는 수직 이동 모드를 연다.</summary>
+    [RequireComponent(typeof(Collider2D))]
+    public sealed class LadderZone : MonoBehaviour
+    {
+        private void Reset()
+        {
+            var zone = GetComponent<Collider2D>();
+            if (zone != null)
+            {
+                zone.isTrigger = true;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            other.GetComponentInParent<PlayerMovement>()?.EnterLadder();
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            other.GetComponentInParent<PlayerMovement>()?.ExitLadder();
+        }
+    }
+}
